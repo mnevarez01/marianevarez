4882806232466a795c8e76393b205ece70628f3e
Maria Nevarez <mnevarez1220@gmail.com>
2020-06-08 17:21:56 -0600

    updated home page

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -9,10 +9,21 @@ const HomePage = () => {
       description: 'An application using ReactJs, Mongoose, Express to create a historical application for those in the Denver Area. You will be able to navigate and learn what happened through the decades. You can also log in and add content and update the content as you see fit.'
     },
     {
-      title: 'Denver Through the Decades',
-      img: 'https://i.imgur.com/U2mi7Kf.png',
-      description: 'An application using ReactJs, Mongoose, Express to create a historical application for those in the Denver Area. You will be able to navigate and learn what happened through the decades. You can also log in and add content and update the content as you see fit.'
-    }
+      title: 'Alewery',
+      img: 'https://i.imgur.com/0hEZeSx.png',
+      description: 'A site that contains all beers from your local breweries! To all breweries have your beers accessible to all!'
+    },
+    {
+      title: 'Quarintainment',
+      img: 'https://i.imgur.com/1DhzhNx.png',
+      description: 'While facing hard times building a website that is interactive and distracting to the mind.'
+    },
+    {
+      title: 'README Generator',
+      img: 'https://i.imgur.com/RN2fyS2.png',
+      description: 'This project will allow you to create a README.MD with little effort.',
+      link: 'https://github.com/mnevarez01/readMe-generator'
+    },
   ]
 
   return (
